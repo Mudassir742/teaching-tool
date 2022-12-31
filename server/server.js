69a4c958baa4d99913da5bf5f577f25495cf6a58
Mudassir742 <mudassirwaheed242@gmail.com>
2022-12-31 22:32:56 +0500

    set for deployement

--- server/server.js
+++ server/server.js
@@ -1,22 +1,27 @@
-const express = require('express');
-const fs = require('fs');
-const { execSync } = require('child_process');
+require("dotenv").config(".env");
+const express = require("express");
+const fs = require("fs");
+const { execSync } = require("child_process");
 const app = new express();
-const PORT = 5000;
+
+const PORT = process.env.PORT || 5000;
 
 const fetchWebsite = (url) => {
-  execSync(`wget -q -O - ${url} > site.html`,
-    (error, stdout, stderr) => {
-      if (error !== null) {
-        return false;
-      }
+  execSync(`wget -q -O - ${url} > site.html`, (error, stdout, stderr) => {
+    if (error !== null) {
+      return false;
+    }
   });
-}
+};
 
-app.get('/', async (req, res) => {
-  fs.writeFileSync('site.html', '', () => console.log('Created site.html'));
-  fs.createReadStream('site.html').pipe(res);
-  fetchWebsite('https://clideo.com/editor/');
+app.get("/", async (req, res) => {
+  fs.writeFileSync("site.html", "", () => console.log("Created site.html"));
+  fs.createReadStream("site.html").pipe(res);
+  fetchWebsite("https://clideo.com/editor/");
 });
 
-app.listen(PORT, () => {console.log("Listening at port: ", PORT)} )
+app.listen(PORT, () => {
+  console.log("Listening at port: ", PORT);
+});
+
+module.exports = app;
